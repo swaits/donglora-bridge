bbe0239ab97b08faccfd5adf4ada851d5b61d822
Stephen Waits <steve@waits.net>
2026-04-07 14:57:17 -0600

    replace repo contents with redirect to donglora org

--- fuzz/fuzz_targets/fuzz_decode_peer_list.rs
+++
@@ -1,7 +0,0 @@
-#![no_main]
-use libfuzzer_sys::fuzz_target;
-
-fuzz_target!(|data: &[u8]| {
-    // decode_peer_list must never panic on arbitrary input.
-    let _ = donglora_bridge::gossip::decode_peer_list(data);
-});
--- fuzz/fuzz_targets/fuzz_gossip_frame_decode.rs
+++
@@ -1,7 +0,0 @@
-#![no_main]
-use libfuzzer_sys::fuzz_target;
-
-fuzz_target!(|data: &[u8]| {
-    // GossipFrame::decode must never panic on arbitrary input.
-    let _ = donglora_bridge::packet::GossipFrame::decode(data);
-});
